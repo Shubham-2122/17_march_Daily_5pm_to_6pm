3222cc74a22b8b4a9d1c948ac1a5ba5b6db9f7bb
Shubham-2122 <shubhamjadav.tops@gmail.com>
2025-05-06 17:48:31 +0530

    js

---
+++ Javascript/ex.js
@@ -0,0 +1,15 @@
+// console.log("hello External js");
+// document.write("HELLO")
+
+
+// document.getElementById("h1").innerText = "hello javscript"
+
+// document.getElementById("h1").innerHTML = "<i>hello javscript</i>"
+
+// bom :- browser object model
+
+
+// alert("pls required data")
+// confirm("Are your sure?")
+// prompt("5 + 5 => ?")
+// print()
